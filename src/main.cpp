6f843e63bba7d8713b8d7406157b76bb14073842
Rohit Nimkar <nehalnimkar@gmail.com>
2022-11-29 16:56:42 +0530

    :sparkles: initial commit

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stm32f1xx.h>
+#include <gpio.hpp>
+
+
+void ms_delay(int ms)
+{
+  while (ms-- > 0)
+  {
+    volatile int x = 500;
+    while (x-- > 0)
+      __asm("nop");
+  }
+}
+
+int main(void)
+{
+  GPIO::enable_PortC();
+  GPIO::setMode(GPIOC, GPIO::PIN_13,GPIO::OUTPUT_2MHZ);
+  GPIO::setConfig(GPIOC, GPIO::PIN_13,GPIO::OUTPUT_PUSH_PULL);
+  while (1)
+  {
+    GPIO::toggle(GPIOC, GPIO::PIN_13);
+    ms_delay(500U);
+  }
+}
